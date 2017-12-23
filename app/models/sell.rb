362938ed7a95310d94085b704105655a12533e35
Diego Ramires <goncalves.ramires@gmail.com>
2017-12-22 23:53:30 -0200

    Add validations to model and change form sell view

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -3,6 +3,15 @@ class Sell < ApplicationRecord
   enum status: { finished: 0, canceled: 1 }
 
   validates :client, presence: true
+  
+  belongs_to :discount
+  belongs_to :client
+
+  has_many :sell_products
+  has_many :products, through: :sell_products
+  
+  has_many :sell_services
+  has_many :services, through: :sell_services
 
   def fae_display_field
     id
@@ -12,6 +21,4 @@ class Sell < ApplicationRecord
     order(:id)
   end
 
-  belongs_to :discount
-  belongs_to :client
 end
