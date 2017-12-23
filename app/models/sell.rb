fba300e9dec2460e9482dbb1b4702af2f3a4046e
Diego Ramires <goncalves.ramires@gmail.com>
2017-12-23 09:35:47 -0200

    Update model sell to include total

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,17 +1,39 @@
 class Sell < ApplicationRecord
-  include Fae::BaseModelConcern
-  enum status: { finished: 0, canceled: 1 }
+include Fae::BaseModelConcern
+enum status: { finalizada: 0, cancelada: 1 }
+validates :client, presence: true
 
-  validates :client, presence: true
+belongs_to :discount
+belongs_to :client
 
-  def fae_display_field
-    id
+has_many :sell_products
+has_many :products, through: :sell_products
+
+has_many :sell_services
+has_many :services, through: :sell_services
+
+before_save :set_total
+
+def fae_display_field
+  id
+end
+
+def self.for_fae_index
+  order(:id)
+end
+
+private
+
+def set_total
+  total = 0
+  self.products.each {|p| total += p.price }
+  self.services.each {|s| total += s.price }
+
+  if self.discount.present?
+    total = total - self.discount.value
   end
 
-  def self.for_fae_index
-    order(:id)
-  end
-
-  belongs_to :discount
-  belongs_to :client
+  total = (total >= 0)? total : 0
+  self.total = total
+end
 end
